059b0de15f94c2ba69f6ce5540beebc3d428e7de
Amankaium <kaium@list.ru>
2020-10-08 21:22:07 +0600

    added receiver messages to homepage

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Message
 
 
 def homepage(request):
-    return render(request, "homepage.html")
+    messages = Message.objects.filter(to_user=request.user)
+    return render(request, "homepage.html", {"messages": messages})
