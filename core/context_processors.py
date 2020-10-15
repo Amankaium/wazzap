c41ea4609726a4d71c041846db60d85c7e89140e
Amankaium <kaium@list.ru>
2020-10-15 19:25:29 +0600

    fixed context processor bug

--- core/context_processors.py
+++ core/context_processors.py
@@ -3,8 +3,11 @@ from django.db.models import Q
 
 
 def chats(request):
-    chats = Chat.objects.filter(
-        Q(message__from_user=request.user) |
-        Q(message__to_user=request.user)
-    ).distinct()
+    if request.user.is_authenticated:
+        chats = Chat.objects.filter(
+            Q(message__from_user=request.user) |
+            Q(message__to_user=request.user)
+        ).distinct()
+    else:
+        chats = []
     return {"chats": chats}
